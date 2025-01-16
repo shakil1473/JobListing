637d97cff1c876a1aa1890c328b1199415838256
shakil mahmud <shakil1473@gmail.com>
2025-01-16 22:13:02 +0100

    Just created project
    Download from spring initializr and uploaded

---
+++ src/test/java/com/shakil/joblisting/JoblistingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakil.joblisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoblistingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
